ac141bc9f747f487f89cdf94fb3bc407509ee0a1
gabor.molnar <gabor.molnar@epassi.fi>
2019-02-24 14:53:25 +0200

    format

--- src/api/middleware/LoggingMiddleware.js
+++ src/api/middleware/LoggingMiddleware.js
@@ -3,11 +3,12 @@ export default class LoggingMiddleware {
   addLogging(req, res, next) {
     res.on('finish', () => {
       console.log('req', JSON.stringify({
-        request: {ip: req.ip,
-        originalUrl: req.originalUrl,
-        method: req.method,
-        params: req.params,
-        body: req.body,
+        request: {
+          ip: req.ip,
+          originalUrl: req.originalUrl,
+          method: req.method,
+          params: req.params,
+          body: req.body,
         },
         response: {
           statusCode: res.statusCode,
@@ -15,7 +16,6 @@ export default class LoggingMiddleware {
       }));
     });
     
-
     return next();
   }
 }
